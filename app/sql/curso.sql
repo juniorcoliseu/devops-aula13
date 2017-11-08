310e1d8d3d18d308580807a9676c7c6d6acfc616
juniorcoliseu <30847674+juniorcoliseu@users.noreply.github.com>
2017-11-08 19:51:33 -0200

    Delete curso.sql

--- app/sql/curso.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-CREATE TABLE `app_curso` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `nome` varchar(200) NOT NULL, `periodo` varchar(50) NOT NULL, `instituicao` varchar(200) NOT NULL);
-
-COMMIT;
-
